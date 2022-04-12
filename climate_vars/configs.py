b65fd0154fcfb4510205e30cb9cd20d274a568d5
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-04-12 16:59:24 +0100

    Move manual parsing to specific library

--- climate_vars/configs.py
+++ climate_vars/configs.py
@@ -7,7 +7,6 @@ spec_occurence_input = os.path.join(climate_inputs_path, 'cleaned_species_occure
 
 climate_temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 
-
 climate_output_path = resource_filename(__name__, 'outputs')
 summary_output_csv = os.path.join(climate_output_path,'occurence_summary.csv')
 if not os.path.isdir(climate_inputs_path):
