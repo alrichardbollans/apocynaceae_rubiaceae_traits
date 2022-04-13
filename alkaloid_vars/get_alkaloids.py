795349e4ed0a9966ed0314d2bca86a5efde16fe3
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-04-13 10:35:58 +0100

    Better include manually collected alkaloids

--- alkaloid_vars/get_alkaloids.py
+++ alkaloid_vars/get_alkaloids.py
@@ -4,6 +4,8 @@ import pandas as pd
 from pkg_resources import resource_filename
 
 from cleaning import compile_hits
+
+from manually_collected_data import encoded_traits_csv
 from metabolite_vars import rub_apoc_alkaloid_hits_output_csv
 from powo_searches import search_powo
 
@@ -13,6 +15,7 @@ _inputs_path = resource_filename(__name__, 'inputs')
 ### Temp outputs
 _temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 _powo_search_temp_output_accepted_csv = os.path.join(_temp_outputs_path, 'powo_alkaloids_accepted.csv')
+_manual_hit_temp_output = os.path.join(_temp_outputs_path, 'manual_alkaloids_accepted.csv')
 
 ### Outputs
 _output_path = resource_filename(__name__, 'outputs')
@@ -25,6 +28,16 @@ def get_powo_alkaloids():
                 filters=['species', 'infraspecies'])
 
 
+def get_manual_hits():
+    trait_table = pd.read_csv(encoded_traits_csv)
+    alk_hits = trait_table[trait_table['Alkaloids'] == 1]
+    alk_hits = alk_hits[[
+        'Alkaloids', 'Accepted_Name', 'Accepted_Species', 'Accepted_Species_ID', 'Accepted_ID', 'Accepted_Rank',
+        'Family']]
+    alk_hits['Source'] = 'Manual'
+    alk_hits.to_csv(_manual_hit_temp_output)
+
+
 def main():
     if not os.path.isdir(_temp_outputs_path):
         os.mkdir(_temp_outputs_path)
@@ -32,11 +45,11 @@ def main():
         os.mkdir(_output_path)
     get_powo_alkaloids()
     powo_hits = pd.read_csv(_powo_search_temp_output_accepted_csv)
-    alk_hits = pd.read_csv(rub_apoc_alkaloid_hits_output_csv)
+    manual_hits = pd.read_csv(_manual_hit_temp_output)
+    alk_metabolite_hits = pd.read_csv(rub_apoc_alkaloid_hits_output_csv)
 
-    compile_hits([powo_hits, alk_hits], output_alkaloid_csv)
+    compile_hits([powo_hits, alk_metabolite_hits, manual_hits], output_alkaloid_csv)
 
 
 if __name__ == '__main__':
-
     main()
