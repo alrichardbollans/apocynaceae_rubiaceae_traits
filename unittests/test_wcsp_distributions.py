46de24839cd661393f3e14e99b5ef02012ef6fae
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-05-13 19:11:35 +0100

    Fix synonym ids

--- unittests/test_wcsp_distributions.py
+++ unittests/test_wcsp_distributions.py
@@ -12,9 +12,9 @@ class MyTestCase(unittest.TestCase):
 
         distros_df = pd.read_csv(distributions_csv, index_col=0)
         distros_df.set_index('kew_id', inplace=True)
-        self.assertEqual("['ANG', 'NAM']", distros_df.at['76360-1', 'iso3_codes'], msg=f'76360-1')
-        self.assertEqual("['SAM']", distros_df.at['77164465-1', 'iso3_codes'], msg=f'77164465-1')
-        self.assertEqual("['CHC', 'CHT']", distros_df.at['756598-1', 'iso3_codes'], msg=f'756598-1')
+        self.assertEqual("['ANG', 'NAM']", distros_df.at['76360-1', 'tdwg3_codes'], msg=f'76360-1')
+        self.assertEqual("['SAM']", distros_df.at['77164465-1', 'tdwg3_codes'], msg=f'77164465-1')
+        self.assertEqual("['CHC', 'CHT']", distros_df.at['756598-1', 'tdwg3_codes'], msg=f'756598-1')
 
 if __name__ == '__main__':
     unittest.main()
