1de8d35d951eb8b5e3e659a814fd5108488181ef
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-05-16 11:57:46 +0100

    Output acc names separately

--- climate_vars/compilation_methods.py
+++ climate_vars/compilation_methods.py
@@ -2,14 +2,15 @@ import os
 
 import numpy as np
 import pandas as pd
-from automatchnames import get_accepted_info_from_names_in_column
+from automatchnames import get_accepted_info_from_names_in_column, get_accepted_info_from_ids_in_column
 from pkg_resources import resource_filename
 
 from large_file_storage import large_folders
 
 _inputs_path = os.path.join(large_folders, 'occ_climate_vars/')
 clean_occurences_with_clim_vars_csv = os.path.join(_inputs_path, 'occ_with_climate_vars.csv')
-occurences_with_accepted_names_csv = os.path.join(_inputs_path, 'occurences_with_accepted_names.csv')
+_occurrences_with_accepted_names_csv = os.path.join(_inputs_path, 'occurences_with_accepted_names.csv')
+occurrences_with_clim_and_accepted_names_csv = os.path.join(_inputs_path, 'occurrences_with_clim_and_accepted_names.csv')
 
 _output_path = resource_filename(__name__, 'outputs')
 compiled_climate_vars_csv = os.path.join(_output_path, 'compiled_climate_vars.csv')
@@ -28,13 +29,13 @@ climate_vars_and_names = {'Beck_KG_V1_present': 'Beck_KG_V1_present',
                           'CHELSA_bio16_1981.2010_V.2.1': 'bio16',
                           'CHELSA_bio17_1981.2010_V.2.1': 'bio17',
                           'gmted_breakline': 'breakline_elevation',
-                          'gmted_elevation': 'mean_elevation',
+                          'gmted_elevation': 'elevation',
                           'gmted_slope': 'slope',
-                          'nitrogen_0.5cm_mean': 'soil_nitrogen',
-                          'phh2o_0.5cm_mean': 'soil_ph',
-                          'soc_0.5cm_mean': 'soil_soc',
+                          'soil_nitrogen': 'soil_nitrogen',
+                          'soil_ph': 'soil_ph',
+                          'soil_soc': 'soil_soc',
                           'soil_depth': 'soil_depth',
-                          'soil_ocs_0.30cm_mean': 'soil_ocs',
+                          'soil_ocs': 'soil_ocs',
                           'water_capacity': 'water_capacity',
                           'decimalLatitude': 'latitude',
                           'decimalLongitude': 'longitude'
@@ -42,17 +43,21 @@ climate_vars_and_names = {'Beck_KG_V1_present': 'Beck_KG_V1_present',
 # All final variables
 all_climate_names = list(climate_vars_and_names.values()) + ['koppen_geiger_mode',
                                                              'koppen_geiger_all']
+all_climate_names.remove('Beck_KG_V1_present')
 families_in_occurrences = ['Apocynaceae', 'Rubiaceae', 'Celastraceae']
 
 
 def read_and_clean_occurences() -> pd.DataFrame:
-    # TODO: read and output without vars
+    """
+    Get accepted info for occurrences. This only needs updating with new occurrences
+    rather than when new cliamte data is added
+    :return:
+    """
     # TODO: remove close occs re: autocorrelation
-    clim_occ_df = pd.read_csv(clean_occurences_with_clim_vars_csv)[
-        ['species', 'fullname', 'countryCode', 'coordinateUncertaintyInMeters',
-         'year', 'individualCount', 'gbifID', 'basisOfRecord', 'institutionCode', 'establishmentMeans',
-         'is_cultivated_observation', 'sourceID'] + list(climate_vars_and_names.keys())]
-    clim_occ_df.rename(columns=climate_vars_and_names, inplace=True)
+
+    clim_occ_df = pd.read_csv(clean_occurences_with_clim_vars_csv)
+    print(clim_occ_df.columns)
+
     # Coord uncertainty 20000
     clean_df = clim_occ_df[clim_occ_df['coordinateUncertaintyInMeters'] <= 20000]
 
@@ -60,27 +65,42 @@ def read_and_clean_occurences() -> pd.DataFrame:
     clean_df = clean_df[clean_df['year'] >= 1945]
 
     ## long and lat
-    clean_df = clean_df[~((clean_df['longitude'] == 0) & (clean_df['latitude'] == 0))]
+    clean_df = clean_df[~((clean_df['decimalLongitude'] == 0) & (clean_df['decimalLatitude'] == 0))]
 
-    clean_df = clean_df[(clean_df['longitude'] != clean_df['latitude'])]
+    clean_df = clean_df[(clean_df['decimalLongitude'] != clean_df['decimalLatitude'])]
 
     # na lat long
-    clean_df = clean_df[~((clean_df['longitude'].isna()) | (clean_df['latitude'].isna()))]
+    clean_df = clean_df[~((clean_df['decimalLongitude'].isna()) | (clean_df['decimalLatitude'].isna()))]
 
     ### CLimate values
 
     acc_df = get_accepted_info_from_names_in_column(clean_df, 'fullname',
                                                     families_of_interest=families_in_occurrences)
-    acc_df.to_csv(occurences_with_accepted_names_csv)
+
+    acc_df = acc_df[['fullname', 'Accepted_Name', 'Accepted_ID', 'Accepted_Rank',
+                     'Accepted_Species', 'Accepted_Species_ID']]
+    acc_df.to_csv(_occurrences_with_accepted_names_csv)
 
     return acc_df
 
 
-def get_climate_df(clean_acc_df: pd.DataFrame):
-    print(clean_acc_df.head())
-    print(clean_acc_df.columns)
+def get_climate_df():
+    occ_df = pd.read_csv(clean_occurences_with_clim_vars_csv)[
+        ['species', 'fullname', 'countryCode', 'coordinateUncertaintyInMeters',
+         'year', 'individualCount', 'gbifID', 'basisOfRecord', 'institutionCode', 'establishmentMeans',
+         'is_cultivated_observation', 'sourceID'] + list(climate_vars_and_names.keys())]
+    occ_df.rename(columns=climate_vars_and_names, inplace=True)
+    print(occ_df.head())
+    print(occ_df.columns)
+
+    # Read accepted info and merge with climate data
+    acc_info = pd.read_csv(_occurrences_with_accepted_names_csv).drop_duplicates(subset='fullname')
+    acc_occ_df = pd.merge(occ_df, acc_info, on='fullname')
+    # Output occurrences with climate vars and accepted names
+    acc_occ_df.to_csv(occurrences_with_clim_and_accepted_names_csv)
+
     dfs = []
-    grouped = clean_acc_df.groupby(['fullname'])
+    grouped = acc_occ_df.groupby(['Accepted_ID'])
     for c in list(climate_vars_and_names.values()):
         # We take median of occurrences in order to mitigate outliers
         # This still has the possible issue of being biased towards where people take samples
@@ -89,24 +109,26 @@ def get_climate_df(clean_acc_df: pd.DataFrame):
         dfs.append(avg)
 
     for df in dfs:
-        print(len(df))
-    merged = pd.merge(dfs[0], dfs[1], on='fullname')
+        if len(df) != len(dfs[0]):
+            print(len(df))
+            print(len(dfs[0]))
+            raise ValueError
+    merged = pd.merge(dfs[0], dfs[1], on='Accepted_ID')
     for i in range(len(dfs)):
         if i > 1:
-            merged = pd.merge(merged, dfs[i], on='fullname')
+            merged = pd.merge(merged, dfs[i], on='Accepted_ID')
 
     # Get mode of koppengeiger classification.
     # In case of multiple modes, select one at random
-    merged['koppen_geiger_mode'] = clean_acc_df.groupby([clean_acc_df['fullname']])['Beck_KG_V1_present'].agg(
+    merged['koppen_geiger_mode'] = acc_occ_df.groupby(['Accepted_ID'])['Beck_KG_V1_present'].agg(
         lambda x: np.random.choice(x.mode(dropna=True)))
 
-    merged['koppen_geiger_all'] = clean_acc_df.groupby([clean_acc_df['fullname']])[
+    merged['koppen_geiger_all'] = acc_occ_df.groupby(['Accepted_ID'])[
         'Beck_KG_V1_present'].unique().apply(
         list).values
 
-    acc_info = clean_acc_df[['fullname', 'Accepted_Name', 'Accepted_ID', 'Accepted_Rank',
-                             'Accepted_Species', 'Accepted_Species_ID']].drop_duplicates(subset='fullname')
-    out_df = pd.merge(merged, acc_info, on='fullname')
+    merged.drop(columns='Beck_KG_V1_present', inplace=True)
+    out_df = pd.merge(merged, acc_info, on='Accepted_ID')
 
     out_df.to_csv(compiled_climate_vars_csv)
 
@@ -150,9 +172,10 @@ def test_occs():
 
 def main():
     # test_occs()
-    acc_occ_df = read_and_clean_occurences()
-    acc_df = pd.read_csv(occurences_with_accepted_names_csv)
-    get_climate_df(acc_df)
+
+    # read_and_clean_occurences()
+
+    get_climate_df()
 
 
 if __name__ == '__main__':
--- climate_vars/summarise_occurences.py
+++ climate_vars/summarise_occurences.py
@@ -3,7 +3,7 @@ import os.path
 import pandas as pd
 from taxa_lists import get_all_taxa
 
-from climate_vars import summary_output_csv, climate_output_path, occurences_with_accepted_names_csv
+from climate_vars import summary_output_csv, climate_output_path, occurrences_with_clim_and_accepted_names_csv
 
 
 def summarise():
@@ -11,13 +11,14 @@ def summarise():
     families = ['Apocynaceae', 'Rubiaceae']
     acc_taxa = get_all_taxa(families_of_interest=families, accepted=True,
                             ranks=['Species', 'Subspecies', 'Variety'])
-    clean_occ_df = pd.read_csv(occurences_with_accepted_names_csv)
+    clean_occ_df = pd.read_csv(occurrences_with_clim_and_accepted_names_csv)
 
     clean_occ_df.describe().to_csv(os.path.join(climate_output_path, 'data_summary.csv'))
     unique_taxa = clean_occ_df['Accepted_Name'].unique()
     unique_taxa_in_fams = [x for x in unique_taxa if x in acc_taxa['accepted_name'].values]
 
-    out_dict = {'Total Taxa': [len(acc_taxa.index)], 'Found Taxa': [len(unique_taxa_in_fams)],
+    out_dict = {'Total Occurrences': [len(clean_occ_df.index)], 'Total Taxa': [len(acc_taxa.index)],
+                'Found Taxa': [len(unique_taxa_in_fams)],
                 'Percent': [float(len(unique_taxa_in_fams)) / len(acc_taxa.index)]}
     out_df = pd.DataFrame(out_dict)
     out_df.to_csv(summary_output_csv)
