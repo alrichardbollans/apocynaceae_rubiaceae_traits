d09ab37a9dcc1ec91c98b2ed1454063578edb28d
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-05-18 12:33:14 +0100

    Improve raster generation and extraction.

--- climate_vars/append_from_rasters.R
+++ climate_vars/append_from_rasters.R
@@ -3,15 +3,17 @@ library(rjson)
 library(dplyr)
 
 configs = rjson::fromJSON(file = '../large_file_storage/my_directories.json')
-
+source(here::here('helper_functions.R'))
 read_rasters <- function(){
   prep_rasters=list()
-  raster_files = list.files(path='temp_outputs/prepared rasters', pattern='*.tiff$')
+  raster_files = list.files(path=file.path('temp_outputs','prepared rasters'), pattern='*.tiff$')
   for (r in raster_files){
-    print(r)
-    prepped_raster = rast(file.path('temp_outputs/prepared rasters',r))
+    
+    prepped_raster = rast(file.path('temp_outputs','prepared rasters',r))
+    summarise_raster(prepped_raster)
     prep_rasters[[length(prep_rasters)+1]]<-prepped_raster
   }
+  
   return(prep_rasters)
 }
 
@@ -27,19 +29,118 @@ occ_df_with_duplicates = rbind(occ_df_with_duplicates,varis_df)
 
 occ_df = dplyr::distinct(occ_df_with_duplicates, gbifID, .keep_all = TRUE)
 
+# Save some RAM
+rm(occ_df_with_duplicates)
+rm(species_df)
+rm(subspecies_df)
+rm(varis_df)
+gc()
 append_var_to_df <- function(df,reaggragated_raster){
+  
   # Update names
   names(reaggragated_raster) = varnames(reaggragated_raster)
-  var_values = data.frame(extract(reaggragated_raster, df[, c("decimalLongitude", "decimalLatitude")], factors=FALSE))
-  bound_df = cbind(df, var_values)
+  print(names(reaggragated_raster))
+  lat_long_vect <- terra::vect(df,geom= c("decimalLongitude", "decimalLatitude"), crs="+proj=longlat")
+  reprojected_vect <- terra::project(lat_long_vect, crs(reaggragated_raster))
   
+  var_values = data.frame(extract(reaggragated_raster, reprojected_vect))
+
+  bound_df = cbind(df, var_values)
+  bound_df <- bound_df %>% select(-ID)
   return(bound_df)
 }
 
+# Need to restart R each time to clean memory
+r1 = prepared_rasters[[1]]
+occ_df<-append_var_to_df(occ_df,r1)
+.rs.restartR()
 
-for (r in prepared_rasters){
-  print(names(r))
-  occ_df<-append_var_to_df(occ_df,r)
-}
-occ_df <- occ_df %>% select(-ID)
+prepared_rasters = read_rasters()
+r2 = prepared_rasters[[2]]
+occ_df<-append_var_to_df(occ_df,r2)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r3 = prepared_rasters[[3]]
+occ_df<-append_var_to_df(occ_df,r3)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r4 = prepared_rasters[[4]]
+occ_df<-append_var_to_df(occ_df,r4)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r5 = prepared_rasters[[5]]
+occ_df<-append_var_to_df(occ_df,r5)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r6 = prepared_rasters[[6]]
+occ_df<-append_var_to_df(occ_df,r6)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r7 = prepared_rasters[[7]]
+occ_df<-append_var_to_df(occ_df,r7)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r8 = prepared_rasters[[8]]
+occ_df<-append_var_to_df(occ_df,r8)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r9 = prepared_rasters[[9]]
+occ_df<-append_var_to_df(occ_df,r9)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r10 = prepared_rasters[[10]]
+occ_df<-append_var_to_df(occ_df,r10)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r11 = prepared_rasters[[11]]
+occ_df<-append_var_to_df(occ_df,r11)
 write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
+.rs.restartR()
+
+
+prepared_rasters = read_rasters()
+r12 = prepared_rasters[[12]]
+occ_df<-append_var_to_df(occ_df,r12)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r13 = prepared_rasters[[13]]
+occ_df<-append_var_to_df(occ_df,r13)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r14 = prepared_rasters[[14]]
+occ_df<-append_var_to_df(occ_df,r14)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r15 = prepared_rasters[[15]]
+occ_df<-append_var_to_df(occ_df,r15)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r16 = prepared_rasters[[16]]
+occ_df<-append_var_to_df(occ_df,r16)
+write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r17 = prepared_rasters[[17]]
+occ_df<-append_var_to_df(occ_df,r17)
+.rs.restartR()
+
+prepared_rasters = read_rasters()
+r18 = prepared_rasters[[18]]
+occ_df<-append_var_to_df(occ_df,r18)
+write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
+
+
--- climate_vars/prepare_rasters.R
+++ climate_vars/prepare_rasters.R
@@ -2,15 +2,15 @@ library(terra)
 library(rjson)
 
 configs = rjson::fromJSON(file = '../large_file_storage/my_directories.json')
-
+source(here::here('helper_functions.R'))
 import_raster <- function(filename){
   #Linux
-  #str_name<-paste(configs$raster_download,filename,sep='/')
+  str_name<-paste(configs$raster_download,filename,sep='/')
   # Windows
-  str_name<-paste('C:\\Users\\ari11kg\\Downloads\\rasters',filename,sep='\\')
+  #str_name<-paste('C:\\Users\\ari11kg\\Downloads\\rasters',filename,sep='\\')
   print(str_name)
   
-  imported_raster=rast(str_name)
+  imported_raster=terra::rast(str_name)
   print(imported_raster)
   return(imported_raster)
 }
@@ -27,7 +27,7 @@ agg_raster <- function(original_raster,desired_resolution){
   fact = desired_resolution/res(original_raster)
   print(res(original_raster))
   print(fact)
-  original_raster.aggregate <- aggregate(original_raster,fact=fact)
+  original_raster.aggregate <- terra::aggregate(original_raster,fact=fact, na.rm=TRUE)
   print(res(original_raster.aggregate))
   return (original_raster.aggregate)
 }
@@ -37,19 +37,7 @@ agg_deg_raster <- function(original_raster){
 
 agg_m_raster<-function(original_raster){
   agg_raster(original_raster,desired_m_resolution)
-}
 
-
-mode_fn <- function(x){
-  # Get modes without NAs and use pick randomly from ties
-  modes = c(statip::mfv(x, na_rm=TRUE))
-  if(length(modes)==1){
-    return(modes)
-  }
-  else{
-    return(sample(modes,size = 1))
-  }
-  
 }
 
 agg_kg_raster <- function(original_raster){
@@ -62,10 +50,14 @@ agg_kg_raster <- function(original_raster){
   return (aggregated)
 }
 
-project_rast <- function(original_raster, template_raster, new_name= NULL){
+project_and_output_rast <- function(original_raster, template_raster, new_name= NULL){
   print('old raster:')
   print(original_raster)
+  
+
   aggregated <- terra::project(original_raster,template_raster)
+  
+  
   
   if (!is.null(new_name)){
     names(aggregated) = new_name
@@ -76,14 +68,55 @@ project_rast <- function(original_raster, template_raster, new_name= NULL){
   return (aggregated)
 }
 
-summarise_raster <- function(r){
-  print('Names:')
-  print(names(r))
-  print('Minmax:')
-  print(minmax(r))
-  print('Res:')
-  print(res(r))
+agg_and_output_deg_rast <-function(original_raster, new_name= NULL){
+  print('old raster:')
+  print(original_raster)
+  
+  
+  aggregated <- agg_deg_raster(original_raster)
+  
+  if (!is.null(new_name)){
+    names(aggregated) = new_name
+  }
+  write_to_temp_outputs(aggregated)
+  print('new raster:')
+  print(aggregated)
+  return (aggregated)
 }
+
+agg_and_output_m_rast <-function(original_raster, new_name= NULL){
+  print('old raster:')
+  print(original_raster)
+  
+  
+  aggregated <- agg_m_raster(original_raster)
+  
+  if (!is.null(new_name)){
+    names(aggregated) = new_name
+  }
+  write_to_temp_outputs(aggregated)
+  print('new raster:')
+  print(aggregated)
+  return (aggregated)
+}
+
+agg_10kmsoil_and_output <-function(original_raster, new_name= NULL){
+  print('old raster:')
+  print(original_raster)
+  
+  
+  aggregated <- agg_raster(original_raster,0.2)
+  
+  if (!is.null(new_name)){
+    names(aggregated) = new_name
+  }
+  write_to_temp_outputs(aggregated)
+  print('new raster:')
+  print(aggregated)
+  return (aggregated)
+}
+
+
 write_to_temp_outputs <- function(r){
   
   summarise_raster(r)
@@ -104,47 +137,41 @@ average_layers <- function(rasters){
   return(app(rasters, terra::mean))
 }
 
-# First get raster in desired format
+# First get raster in desired format (can then project other rasters to format if needed)
 chelsa_bio1 = import_raster('CHELSA_bio1_1981-2010_V.2.1.tif')
-chelsa_bio1.aggregated = agg_deg_raster(chelsa_bio1)
-write_to_temp_outputs(chelsa_bio1.aggregated)
-
-# Then project other rasters to format
-elevation = import_raster('mn30_grd/mn30_grd')
-elevation.aggregated = project_rast(elevation,chelsa_bio1.aggregated,
-                                    new_name = c("gmted_elevation"))
-
-slope = terra::terrain(elevation,v="slope")
-slope.aggregated = project_rast(slope,chelsa_bio1.aggregated,
-                                new_name = c("gmted_slope"))
-
-breakline = import_raster('gmted10_breaklineemph.tif')
-breakline.aggregated = project_rast(breakline,chelsa_bio1.aggregated,
-                                    new_name = c("gmted_breakline"))
+chelsa_bio1.aggregated = agg_and_output_deg_rast(chelsa_bio1, 
+                                                 new_name = c("mean_air_temp"))
 
 chelsa_bio4 = import_raster('CHELSA_bio4_1981-2010_V.2.1.tif')
-chelsa_bio4.aggregated = project_rast(chelsa_bio4,chelsa_bio1.aggregated)
+chelsa_bio4.aggregated = agg_and_output_deg_rast(chelsa_bio4, 
+                                                 new_name = c("temp_seasonality"))
 
 chelsa_bio10 = import_raster('CHELSA_bio10_1981-2010_V.2.1.tif')
-chelsa_bio10 = project_rast(chelsa_bio10,chelsa_bio1.aggregated)
+chelsa_bio10 = agg_and_output_deg_rast(chelsa_bio10, 
+                                       new_name = c("bio10"))
 
 chelsa_bio11 = import_raster('CHELSA_bio11_1981-2010_V.2.1.tif')
-chelsa_bio11.aggregated = project_rast(chelsa_bio11,chelsa_bio1.aggregated)
+chelsa_bio11.aggregated = agg_and_output_deg_rast(chelsa_bio11, 
+                                                  new_name = c("bio11"))
 
 chelsa_bio12 = import_raster('CHELSA_bio12_1981-2010_V.2.1.tif')
-chelsa_bio12.aggregated = project_rast(chelsa_bio12,chelsa_bio1.aggregated)
+chelsa_bio12.aggregated = agg_and_output_deg_rast(chelsa_bio12, 
+                                                  new_name = c("precip_amount"))
 
 chelsa_bio15 = import_raster('CHELSA_bio15_1981-2010_V.2.1.tif')
-chelsa_bio15.aggregated = project_rast(chelsa_bio15,chelsa_bio1.aggregated)
+chelsa_bio15.aggregated = agg_and_output_deg_rast(chelsa_bio15, 
+                                                  new_name = c("precip_seasonality"))
 
 chelsa_bio16 = import_raster('CHELSA_bio16_1981-2010_V.2.1.tif')
-chelsa_bio16.aggregated = project_rast(chelsa_bio16,chelsa_bio1.aggregated)
+chelsa_bio16.aggregated = agg_and_output_deg_rast(chelsa_bio16, 
+                                                  new_name = c("bio16"))
 
 chelsa_bio17 = import_raster('CHELSA_bio17_1981-2010_V.2.1.tif')
-chelsa_bio17.aggregated = project_rast(chelsa_bio17,chelsa_bio1.aggregated)
-
+chelsa_bio17.aggregated = agg_and_output_deg_rast(chelsa_bio17, 
+                                                  new_name = c("bio17"))
+# Soil
 depth_to_bedrock = import_raster('BDTICM_M_10km_ll.tif')
-depth_to_bedrock.aggregated = project_rast(depth_to_bedrock,chelsa_bio1.aggregated,
+depth_to_bedrock.aggregated = agg_10kmsoil_and_output(depth_to_bedrock,
                                            new_name =  c("soil_depth"))
 
 # Soil rasters are averaged over 30cm depth
@@ -153,34 +180,48 @@ wc2 = import_raster('WWP_M_sl2_10km_ll.tif')
 wc3 = import_raster('WWP_M_sl3_10km_ll.tif')
 wc4 = import_raster('WWP_M_sl4_10km_ll.tif')
 water_capacity = average_layers(c(wc1,wc2,wc3,wc4))
-water_capacity.aggregated = project_rast(water_capacity,chelsa_bio1.aggregated,
+water_capacity.aggregated = agg_10kmsoil_and_output(water_capacity,
                                          new_name = c("water_capacity"))
 
 
 soil_ocs = import_raster('ocs_0-30cm_mean_5000.tif')
-soil_ocs.aggregated = project_rast(soil_ocs,chelsa_bio1.aggregated, 
-                                   new_name = c("soil_ocs"))
+soil_ocs.aggregated = agg_and_output_m_rast(soil_ocs,
+                                        new_name = c("soil_ocs"))
 
 ph1 = import_raster('phh2o_0-5cm_mean_5000.tif')
 ph2 = import_raster('phh2o_5-15cm_mean_5000.tif')
 ph3 = import_raster('phh2o_15-30cm_mean_5000.tif')
 homo_ph_soil = average_layers(c(ph1,ph2,ph3))
-ph_soil.aggregated = project_rast(homo_ph_soil,chelsa_bio1.aggregated,
-                                  new_name = c("soil_ph"))
+ph_soil.aggregated = agg_and_output_m_rast(homo_ph_soil,
+                                       new_name = c("soil_ph"))
 
 nit1 = import_raster('nitrogen_0-5cm_mean_5000.tif')
 nit2 = import_raster('nitrogen_5-15cm_mean_5000.tif')
 nit3 = import_raster('nitrogen_15-30cm_mean_5000.tif')
 homo_nit_soil = average_layers(c(nit1,nit2,nit3))
-nit_soil.aggregated = project_rast(homo_nit_soil,chelsa_bio1.aggregated,
-                                   new_name =  c("soil_nitrogen"))
+nit_soil.aggregated = agg_and_output_m_rast(homo_nit_soil,
+                                        new_name =  c("soil_nitrogen"))
+
 
 soc1 = import_raster('soc_0-5cm_mean_5000.tif')
 soc2 = import_raster('soc_5-15cm_mean_5000.tif')
 soc3 = import_raster('soc_15-30cm_mean_5000.tif')
 homo_soc_soil = average_layers(c(soc1,soc2,soc3))
-soc_soil.aggregated = project_rast(homo_soc_soil,chelsa_bio1.aggregated,
-                                   new_name = c("soil_soc"))
+soc_soil.aggregated = agg_and_output_m_rast(homo_soc_soil,
+                                        new_name = c("soil_soc"))
+# Topography
+elevation = import_raster('mn30_grd/mn30_grd')
+elevation.aggregated = agg_and_output_deg_rast(elevation,
+                                    new_name = c("gmted_elevation"))
+
+slope = terra::terrain(elevation,v="slope")
+slope.aggregated = agg_and_output_deg_rast(slope,
+                                new_name = c("gmted_slope"))
+
+breakline = import_raster('be30_grd/be30_grd')
+breakline.aggregated = agg_and_output_deg_rast(breakline,
+                                    new_name = c("gmted_breakline"))
+
 
 # KG needs specific aggregation
 kg = import_raster('Beck_KG_V1_present_0p083.tif')
