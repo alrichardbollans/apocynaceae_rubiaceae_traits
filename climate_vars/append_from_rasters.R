a5de9da663a9890ce1373369dd7a77c283a2b831
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-05-23 15:17:28 +0100

    Separate native and introduced occurrences

--- climate_vars/append_from_rasters.R
+++ climate_vars/append_from_rasters.R
@@ -20,9 +20,16 @@ read_rasters <- function(){
 prepared_rasters = read_rasters()
 
 # Get dataframe containing cleaned occurences
-species_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/cleaned_sp_occurences.csv',sep='/'))
-subspecies_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/cleaned_subsp_occurences.csv',sep='/'))
-varis_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/cleaned_vari_occurences.csv',sep='/'))
+native = TRUE # Whether to look at occurrences from native or introduced regions
+if (native){
+  species_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/native/cleaned_sp_occurences.csv',sep='/'))
+  subspecies_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/native/cleaned_subsp_occurences.csv',sep='/'))
+  varis_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/native/cleaned_vari_occurences.csv',sep='/'))
+} else{
+  species_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/introduced/cleaned_sp_occurences.csv',sep='/'))
+  subspecies_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/introduced/cleaned_subsp_occurences.csv',sep='/'))
+  varis_df = read.csv(paste(configs$large_folders,'plant_occurence_vars/outputs/introduced/cleaned_vari_occurences.csv',sep='/'))
+}
 
 occ_df_with_duplicates = rbind(species_df,subspecies_df)
 occ_df_with_duplicates = rbind(occ_df_with_duplicates,varis_df)
@@ -50,99 +57,112 @@ append_var_to_df <- function(df,reaggragated_raster){
   return(bound_df)
 }
 
-# Need to restart R each time to clean memory
-r1 = prepared_rasters[[1]]
-occ_df<-append_var_to_df(occ_df,r1)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r2 = prepared_rasters[[2]]
-occ_df<-append_var_to_df(occ_df,r2)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r3 = prepared_rasters[[3]]
-occ_df<-append_var_to_df(occ_df,r3)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r4 = prepared_rasters[[4]]
-occ_df<-append_var_to_df(occ_df,r4)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r5 = prepared_rasters[[5]]
-occ_df<-append_var_to_df(occ_df,r5)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r6 = prepared_rasters[[6]]
-occ_df<-append_var_to_df(occ_df,r6)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r7 = prepared_rasters[[7]]
-occ_df<-append_var_to_df(occ_df,r7)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r8 = prepared_rasters[[8]]
-occ_df<-append_var_to_df(occ_df,r8)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r9 = prepared_rasters[[9]]
-occ_df<-append_var_to_df(occ_df,r9)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r10 = prepared_rasters[[10]]
-occ_df<-append_var_to_df(occ_df,r10)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r11 = prepared_rasters[[11]]
-occ_df<-append_var_to_df(occ_df,r11)
-write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
-.rs.restartR()
+for (r in prepared_rasters){
+  occ_df<-append_var_to_df(occ_df,r)
+  gc()
+}
 
 
-prepared_rasters = read_rasters()
-r12 = prepared_rasters[[12]]
-occ_df<-append_var_to_df(occ_df,r12)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r13 = prepared_rasters[[13]]
-occ_df<-append_var_to_df(occ_df,r13)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r14 = prepared_rasters[[14]]
-occ_df<-append_var_to_df(occ_df,r14)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r15 = prepared_rasters[[15]]
-occ_df<-append_var_to_df(occ_df,r15)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r16 = prepared_rasters[[16]]
-occ_df<-append_var_to_df(occ_df,r16)
-write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r17 = prepared_rasters[[17]]
-occ_df<-append_var_to_df(occ_df,r17)
-.rs.restartR()
-
-prepared_rasters = read_rasters()
-r18 = prepared_rasters[[18]]
-occ_df<-append_var_to_df(occ_df,r18)
 names(occ_df)[names(occ_df)=="decimalLatitude"] <- "latitude"
 names(occ_df)[names(occ_df)=="decimalLongitude"] <- "longitude"
-write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/occ_with_climate_vars.csv',sep='/'))
+
+
+if (native){
+  write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/native/occ_with_climate_vars.csv',sep='/'))
+} else{
+  write.csv(occ_df, paste(configs$large_folders,'occ_climate_vars/introduced/occ_with_climate_vars.csv',sep='/'))
+}
+
+##### If need more RAM
+# Need to restart R each time to clean memory
+# r1 = prepared_rasters[[1]]
+# occ_df<-append_var_to_df(occ_df,r1)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r2 = prepared_rasters[[2]]
+# occ_df<-append_var_to_df(occ_df,r2)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r3 = prepared_rasters[[3]]
+# occ_df<-append_var_to_df(occ_df,r3)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r4 = prepared_rasters[[4]]
+# occ_df<-append_var_to_df(occ_df,r4)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r5 = prepared_rasters[[5]]
+# occ_df<-append_var_to_df(occ_df,r5)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r6 = prepared_rasters[[6]]
+# occ_df<-append_var_to_df(occ_df,r6)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r7 = prepared_rasters[[7]]
+# occ_df<-append_var_to_df(occ_df,r7)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r8 = prepared_rasters[[8]]
+# occ_df<-append_var_to_df(occ_df,r8)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r9 = prepared_rasters[[9]]
+# occ_df<-append_var_to_df(occ_df,r9)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r10 = prepared_rasters[[10]]
+# occ_df<-append_var_to_df(occ_df,r10)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r11 = prepared_rasters[[11]]
+# occ_df<-append_var_to_df(occ_df,r11)
+# .rs.restartR()
+#
+#
+# prepared_rasters = read_rasters()
+# r12 = prepared_rasters[[12]]
+# occ_df<-append_var_to_df(occ_df,r12)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r13 = prepared_rasters[[13]]
+# occ_df<-append_var_to_df(occ_df,r13)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r14 = prepared_rasters[[14]]
+# occ_df<-append_var_to_df(occ_df,r14)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r15 = prepared_rasters[[15]]
+# occ_df<-append_var_to_df(occ_df,r15)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r16 = prepared_rasters[[16]]
+# occ_df<-append_var_to_df(occ_df,r16)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r17 = prepared_rasters[[17]]
+# occ_df<-append_var_to_df(occ_df,r17)
+# .rs.restartR()
+#
+# prepared_rasters = read_rasters()
+# r18 = prepared_rasters[[18]]
+# occ_df<-append_var_to_df(occ_df,r18)
+
 
 
