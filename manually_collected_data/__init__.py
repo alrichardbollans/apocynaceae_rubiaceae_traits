8c0d29db6f2a5f7ed3f8894fad1bae20a31d76e7
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-09-05 13:45:50 +0100

    Rename module

--- manually_collected_data/__init__.py
+++ manually_collected_data/__init__.py
@@ -1,5 +1,5 @@
 from .import_trait_csvs import *
-from .encode_features import *
+from .encode_features_after_import import *
 from .parse_morphology import *
 from .parse_steroid_cards import *
 from .parse_alks import *
